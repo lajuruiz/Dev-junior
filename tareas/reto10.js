e76a20cc157110451356938ed369941a56025b67
Laura Juliana Ruiz <laurajulianaru@gmail.com>
2022-09-15 20:44:12 -0500

    mejoranndo codigp

--- tareas/reto10.js
+++ tareas/reto10.js
@@ -1,5 +1,7 @@
 /* 
-NOTA: Se le llama instanciar una clase, crear un objeto o crear una instancia a la acción de crear un nuevo objeto basado en una clase particular. Esta acción la realizamos a través de la palabra clave new, seguida del nombre de la clase, la cuál puede tener parámetros, en cuyo caso se controlarían desde un constructor */
+NOTA: Se le llama instanciar una clase, crear un objeto o crear una instancia a la acción de crear un nuevo objeto basado en una clase particular. 
+Esta acción la realizamos a través de la palabra clave new, seguida del nombre de la clase, la cuál puede tener parámetros, en cuyo caso se 
+controlarían desde un constructor */
 //Crea una clase llamada Punto con sus dos coordenadas X e Y
 
 /* class Punto {
@@ -8,45 +10,61 @@ NOTA: Se le llama instanciar una clase, crear un objeto o crear una instancia a 
         this.y = y;
     }
 }
-const coordenada= new Punto(20,28);
+const coordenada= new Punto(2,8);
+const planos = new Punto (3,5);
 console.log(coordenada)
-console.log(coordenada.x, coordenada.y) */
+console.log(planos)
 
 ///////////////////////////////////////////////////////////////////
-
 
 /* Añade un método constructor para crear puntos fácilmente. Si no se recibe una coordenada,
 su valor será cero. */
 
 /* class Punto {
     constructor(x,y){
-        this.x = x;
-        this.y = y;
 
-        if(x === undefined && y === undefined ){
-            this.x = 0;
-            this.y = 0;
+        if(x){
+           this.x = x; 
+        }
+        else{
+            this.x=0;
+        }
+        if(y){
+           this.y = y; 
+        }
+        else{
+            this.y=0;
         }
     }
 }
 const coordenada= new Punto(20,28);
 console.log(coordenada)
-console.log(coordenada.x, coordenada.y) */
-
+const x = new Punto (0)
+console.log(x)
+const y = new Punto (2)
+console.log(y)
+const m = new Punto ()
+console.log(m)
+ */
 
 /* Crea un método llamado ver para que al imprimir por pantalla un punto aparezca en formato (X,Y) */
 
 /* class Punto {
     constructor(x,y){
-        this.x = x;
-        this.y = y;
 
-        if(x === undefined && y === undefined ){
-            this.x = 0;
-            this.y = 0;
+        if(x){
+           this.x = x; 
+        }
+        else{
+            this.x=0;
+        }
+        if(y){
+           this.y = y; 
+        }
+        else{
+            this.y=0;
         }
     }
-
     ver(){
         console.log(`(${this.x},${this.y})`)
     }
@@ -63,11 +81,21 @@ class Punto {
         this.x = x;
         this.y = y;
 
-        if(x === undefined && y === undefined ){
-            this.x = 0;
-            this.y = 0;
-        }
+
+        if(x){
+            this.x = x; 
+         }
+         else{
+             this.x=0;
+         }
+         if(y){
+            this.y = y; 
+         }
+         else{
+             this.y=0;
+         }
     }
+
     ver(){
         console.log(`(${this.x},${this.y})`)
     }
@@ -75,8 +103,7 @@ class Punto {
     vector(punto){
         let nuevoVectorX = this.x + punto.x;
         let nuevoVectorY = this.y + punto.y;
-        const puntoNuevo = new Punto( nuevoVectorX, nuevoVectorY);
-        return puntoNuevo;
+        return new Punto( nuevoVectorX, nuevoVectorY);
     }
 }
 const coordenada= new Punto(2,8);
@@ -87,4 +114,7 @@ coordenada.ver();
 const puntos = new Punto(12,2)
 console.log(coordenada.vector(puntos));
 
-//Añade un método llamado distancia, que tome otro punto y calcule la distancia entre los dos puntos y la muestre por pantalla. La fórmula es la siguiente:
+//Añade un método llamado distancia, que tome otro punto y calcule la distancia entre los dos 
+//puntos y la muestre por pantalla. La fórmula es la siguiente:
+
+
